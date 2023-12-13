60bec825896ecb4b421d405fd07cd40fa8e9c311
Fallen <amir.amon@mail.ru>
2023-12-14 01:41:31 +0900

    update pipline

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-import os
-from dotenv import load_dotenv
 import aws_cdk as cdk
 
 from aws_cicd_cdk.aws_cicd_cdk_stack import AwsCicdCdkStack
 
-load_dotenv()
 
 app = cdk.App()
 AwsCicdCdkStack(app, "AwsCicdCdkStack",
-                env=cdk.Environment(account=os.getenv('CDK_ACCOUNT'),
-                                    region=os.getenv('CDK_REGION')),
+                env=cdk.Environment(account="241916871697",
+                                    region="ap-northeast-1"),
                 )
 
 app.synth()
