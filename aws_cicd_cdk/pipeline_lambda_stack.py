529ce473056d6d6de32310e4d717bbd27e793476
Fallen <amir.amon@mail.ru>
2023-12-15 01:47:28 +0900

    fix lambda stack

--- aws_cicd_cdk/pipeline_lambda_stack.py
+++ aws_cicd_cdk/pipeline_lambda_stack.py
@@ -5,12 +5,12 @@ from aws_cdk.aws_lambda import Function, Code, Runtime
 
 
 class MyLambdaStack(cdk.Stack):
-    def __init__(self, scope: Construct, construct_id: str, stageName: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str,  **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         Function(self, "PiplineLambdaFunc",
                  runtime=Runtime.PYTHON_3_12,
                  handler="lambda_handler.handler",
                  code=Code.from_asset(os.path.join(os.getcwd(), "lambda")),
-                 environment={"stageName": stageName}
+                 environment={"stageName": construct_id}
                  )
